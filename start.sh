5ac2fee1e5af03a6bb0a948d3b9cd90e6a8fff69
Jasm Sison <buggasnoo@gmail.com>
2010-08-10 18:48:49 +0200

    added more ideas

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 set -e -x
 grep -n user_allow_other /etc/fuse.conf
 mkdir -p ~/mnt/gmailfs-jasm
-python src/gmailfs.py -o allow_root -d 9 /home/jasm/mnt/gmailfs-jasm/ | tee -a log.txt
+python src/gmailfs.py -o allow_root -d 9 /home/jasm/mnt/gmailfs-jasm/ 2>&1 | tee -a log.txt
 
