9c47ee09e26a7bb7cf651ef44f87bcd1d3a61120
Buggaboo <buggasnoo@gmail.com>
2010-08-11 23:55:01 +0200

    added some more stuff to TODO

--- killgmailfs.sh
+++ killgmailfs.sh
@@ -2,4 +2,4 @@
 
 set -e -x
 
-kill $(`which ps` axf | egrep "python.*gmailfs" | grep -v 'grep' | sed 's/^ *\([0-9]\+\).*$/\1/')
+kill $(`which ps` a | `which egrep` "python.*gmailfs" | grep -v 'grep' | sed 's/^ *\([0-9]\+\).*$/\1/')
