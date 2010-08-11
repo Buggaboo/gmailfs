3834fd678234a8ed5c488d9c88c0a1e2268aa0b7
Buggaboo <buggasnoo@gmail.com>
2010-08-11 19:22:56 +0200

    added tidbits to dining philosophers

--- src/dinphil.py
+++ src/dinphil.py
@@ -10,17 +10,22 @@ Effort to gain understanding of python and threads.
 3. Monitor solution, starvation.
 4. Chandy / Misra solution, this one uses message passing to borrow a clean chopstick.
 
-
-The drinking philosophers problem
-souce: http://www.cs.utexas.edu/users/misra/scannedPdf.dir/DrinkingPhil.pdf
+The drinking philosophers problem is the general problem where dining philosophers is an instance.
+source: http://www.cs.utexas.edu/users/misra/scannedPdf.dir/DrinkingPhil.pdf
 """
 
+class Philosopher(object):
+  pass
 
+CLEAN = 0
+DIRTY = 1
+class Fork(object):
+  def __init__(self):
+    self.state = CLEAN
 
-
-def main(*args, **kwargs):
+def main(av):
   pass
 
 if __name__ == '__main__':
-  from sys import args
-  main(args)
+  from sys import argv as av
+  main(av)
